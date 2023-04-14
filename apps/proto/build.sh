af4d29204f768bc4a839e9dd0db844f9e75bfe9c
jimyan <jimyan@tencent.com>
2023-04-14 23:12:35 +0800

    更新proto的build命令

--- apps/proto/build.sh
+++ apps/proto/build.sh
@@ -2,8 +2,8 @@
 
 
 # 生成m1模块的pt文件
-./node_modules/.bin/ts-node bin/gen.ts --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=interface proto/mwp/m1/*.proto
+./node_modules/.bin/ts-node bin/gen.ts --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=interface proto/mwp/**/*.proto
 
-./node_modules/.bin/ts-node bin/gen.ts --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=interface proto/mwp/m2/*.proto
+# ./node_modules/.bin/ts-node bin/gen.ts --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=interface proto/mwp/m2/*.proto
 
 ./node_modules/.bin/tsc -p tsconfig.json
